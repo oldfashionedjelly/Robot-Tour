cb52805b1a296cab3c54667b8635ceb56da609d5
Alexandra Bunch <alexandrabunch@icloud.com>
2023-12-19 16:35:20 -0500

    organization

--- motors.ino
+++ motors.ino
@@ -1,4 +1,6 @@
 #include <AFMotor.h>
+#include <iostream>
+using namespace std;
 
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
@@ -10,22 +12,29 @@ void setup() {
 }
 
 void loop() {
+  init();
+}
+
+void init() {
   motor1.setSpeed(speed(100));
   motor2.setSpeed(speed(100));
   motor3.setSpeed(speed(100));
   motor4.setSpeed(speed(100));
-}
-
-void init () {
-  
+  halfForward();
 }
 
 void leftBlock() {
-
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
 }
 
 void rightBlock() {
-
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD); 
 }
 
 void forwardBlock() {
@@ -43,11 +52,17 @@ void backBlock() {
 }
 
 void halfLeft() {
-
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
 }
 
 void halfRight() {
-  
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);  
 }
 
 void halfForward() {
