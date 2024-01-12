f479c993a6f53400468fc9fe3d343c85f849c01f
Alexandra Bunch <alexandrabunch@icloud.com>
2024-01-11 21:15:13 -0500

    final

--- motors.ino
+++ motors.ino
@@ -1,12 +1,12 @@
 #include <AFMotor.h>
 
-AF_DCMotor motor1(1); //blue FR
-AF_DCMotor motor2(2); //white FL **
-AF_DCMotor motor3(3); //black BL
-AF_DCMotor motor4(4); //red BR **
+AF_DCMotor motor1(1); 
+AF_DCMotor motor2(2); 
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4); 
 
 const int buttonPin = 0;
-int buttonState = 0; //low
+int buttonState = 0; 
 
 void setup() {
   Serial.begin(9600);
@@ -15,47 +15,38 @@ void setup() {
 
 void loop() {
   buttonState = digitalRead(buttonPin); 
-  //low = pressed (0)
-  //high = not pressed (1)
   if (buttonState == LOW) {
-    //start
-    //go forward, turn, etc
-    //Set speed here to stop motors from going before button is pressed
     int speed = 255;
-    motor1.setSpeed(speed); //DO NOT SET SPEED LOWER THAN 110
+    motor1.setSpeed(speed);
     motor2.setSpeed(speed);
     motor3.setSpeed(speed);
     motor4.setSpeed(speed);
-    int period = 1500;
+    int period = 926;
+    int turn = 375;
     forward(period);
-    right(period);
-    back(period);
-    left(period);
+    delay(2000);
+    turnRight(turn);
   }
 }
 
-// int speed (int percent) {
-//   return map(percent, 0, 100, 0, 255);
-// }
-
-void turnLeft() {
+void turnLeft(int period) {
   motor1.run(BACKWARD);
   motor2.run(FORWARD);
   motor3.run(FORWARD);
   motor4.run(BACKWARD);
-  delay(630);
+  delay(period);
   motor1.run(RELEASE);
   motor2.run(RELEASE);
   motor3.run(RELEASE);
   motor4.run(RELEASE);
 }
 
-void turnRight(){
+void turnRight(int period){
   motor1.run(FORWARD);
   motor2.run(BACKWARD);
   motor3.run(BACKWARD);
   motor4.run(FORWARD);
-  delay(630);
+  delay(period);
   motor1.run(RELEASE);
   motor2.run(RELEASE);
   motor3.run(RELEASE);
